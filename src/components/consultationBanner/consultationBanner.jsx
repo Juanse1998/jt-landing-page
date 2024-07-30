dbc08d3391aed5321c88fa50b09ee8ff8d3b7987
juanse <juansesosa98@gmail.com>
2024-07-30 13:48:34 -0300

    Se modifico estilos banner

--- src/components/consultationBanner/consultationBanner.jsx
+++ src/components/consultationBanner/consultationBanner.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 const ConsultationBanner = () => {
   return (
-    <div className="bg-gradient-to-r from-pink-300 to-blue-200 p-8 flex flex-col md:flex-row items-center justify-between">
-      <div className="flex justify-center md:justify-start w-full md:w-auto mb-4 md:mb-0">
-        <img src="./smilepng05.png" alt="Consulta Online" className="w-48 h-auto md:w-96" />
+    <div className="bg-gradient-to-r from-pink-300 to-blue-200 p-8 flex flex-col md:flex-row items-center">
+      <div className="flex justify-center md:justify-start w-full md:w-auto mb-4 md:mb-0 ml-16v ">
+        <img src="./smilepng05.png" alt="Consulta Online" className="w-48 h-auto md:w-96 drop-shadow-2xl " />
       </div>
-      <div className="flex flex-col items-center md:items-start space-y-4 w-full md:w-96 text-center md:text-left">
-        <h1 className="text-2xl md:text-4xl font-bold text-gray-900">CONSULTA GRATUITA ONLINE</h1>
+      <div className="flex flex-col items-center md:items-start space-y-4 w-full md:w-6/12 text-center md:text-left ml-60">
+        <h1 className="text-2xl md:text-5xl font-bold text-gray-900">CONSULTA GRATUITA ONLINE</h1>
         <p className="text-lg md:text-xl text-gray-700">Con nuestra doctora</p>
         <div className="flex space-x-4 justify-center md:justify-start">
           <a href="#" className="text-3xl text-gray-900 hover:text-gray-600">
--- src/components/services/Jobs.jsx
+++ src/components/services/Jobs.jsx
@@ -4,9 +4,15 @@ import Card from './Card';
 
 const Jobs = () => {
   return (
-    <div className="bg-gray-100 py-20">
-      <div className="text-center mb-12">
-        <h1 className="text-4xl font-bold text-gray-800">Nuestros Servicios</h1>
+    <div className="bg-white py-20">
+      <div className="flex justify-center mb-12">
+        <div className="text-center mr-4">
+          <section className="text-3xl font-bold text-white-200">
+            <span className="text-black">Nuestros</span> 
+            <strong className="text-pink-500"> Servicios</strong>
+          </section>
+        </div>
+        <div className="border-b-2 border-gray-400 my-2 w-7/12" />
       </div>
       <div className="container mx-auto px-4 w-10/12">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
