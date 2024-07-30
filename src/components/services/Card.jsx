afbcba854e5bd5c4dad0c330412a5bfdf58a0615
juanse <juansesosa98@gmail.com>
2024-07-30 12:42:46 -0300

    Se modificaron estilos de card

--- src/components/services/Card.jsx
+++ src/components/services/Card.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Card = (props) => {
   return (
-    <div className="bg-white shadow-lg rounded-lg overflow-hidden transform transition-transform hover:scale-105 hover:shadow-xl w-full max-w-xs mx-auto">
+    <div className="bg-white shadow-lg rounded-lg overflow-hidden transform transition-transform hover:scale-105 hover:shadow-xl w-64 h-68 max-w-xs mx-auto">
       <div className="p-4 flex justify-center">
-        <img className="h-32 object-cover rounded-t-lg" src={props.services.img} alt={props.services.title} />
+        <img className="h-24 rounded-t-lg" width={90} src={props.services.img} alt={props.services.title} />
       </div>
-      <div className="p-4">
+      <div className="p-4 text-center">
         <h2 className="text-lg font-semibold text-gray-800 mb-2">{props.services.title}</h2>
-        <p className="text-gray-600 text-sm">{props.services.description}</p>
+        <p className="text-gray-600 text-sm text-center">{props.services.description}</p>
       </div>
     </div>
   );
--- src/components/services/Jobs.jsx
+++ src/components/services/Jobs.jsx
@@ -8,7 +8,7 @@ const Jobs = () => {
       <div className="text-center mb-12">
         <h1 className="text-4xl font-bold text-gray-800">Nuestros Servicios</h1>
       </div>
-      <div className="container mx-auto px-4">
+      <div className="container mx-auto px-4 w-10/12">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
           {data.services.map((serv) => (
             <Card key={serv.id} services={serv} />
